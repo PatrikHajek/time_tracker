aee8272aaa7691e7fca9a306ab65a387e6561ba1
PatrikHajek <hajan21@seznam.cz>
2025-09-06 19:31:01 +0200

    refactor: move Config and Action into config module

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use std::{env, error::Error, process};
-
-use time_tracker::Config;
+use std::{env, error::Error};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    time_tracker::run(config)?;
+    time_tracker::run(&args)?;
     // if let Err(e) = time_tracker::run(config) {
     //     eprintln!("Application error: {e}");
     //     process::exit(1);
